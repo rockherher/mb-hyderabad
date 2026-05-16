f8fd6c5862aa57d9b5ba667d1350b51b0776e3b1
Sameer <sameerbagwan132@gmail.com>
2026-05-16 12:28:55 +0530

    feat: create BookingForm component with validation and Google Forms integration

--- src/components/booking-form.tsx
+++ src/components/booking-form.tsx
@@ -103,7 +103,7 @@ const BookingForm: React.FC = () => {
       <div className="mx-auto bg-[#F8F8F8] rounded-2xl p-4 md:p-8 border border-gray-300">
         <form
           onSubmit={handleSubmit}
-          action="https://script.google.com/macros/s/1rd2cs95U4Zz5KD0_sZEylr2oM_UILg28dXXSmtSme7HskvjFnhW0Ztyh/exec"
+          action="https://script.google.com/macros/s/AKfycbzQUwiMezmCEbhG8Q87OwcUR1mcEl_8wKbJFvoHxeLokM8T46CbAVV0HYZ6CQwbwzyy/exec"
           method="POST"
           className="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-4"
         >
